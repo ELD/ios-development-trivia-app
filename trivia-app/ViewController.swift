d396d10f685de1a604bbf27d6f16ce4e4a52b144
Eric Dattore <edattore@gmail.com>
2016-09-06 11:56:45 -0600

    add initial controls, outlets, and actions

--- trivia-app/ViewController.swift
+++ trivia-app/ViewController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionDifficultySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var previousQuestionButton: UIButton!
+    @IBOutlet weak var nextQuestionButton: UIButton!
+    
+    @IBAction func questionDifficultyChanged(sender: UISegmentedControl) {
+        let difficulty = sender.titleForSegmentAtIndex(sender.selectedSegmentIndex)!
+        print("Question difficulty changed to \(difficulty)")
+    }
+    
+    @IBAction func previousQuestion(sender: UIButton) {
+        print("Previous button pressed")
+    }
+    
+    @IBAction func nextQuestion(sender: UIButton) {
+        print("Next button pressed")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
